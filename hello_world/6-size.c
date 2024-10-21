f09723e974a394387e195b4e0a43bddfe0e86dfb
sleanz <nils.8@hotmail.fr>
2024-10-21 12:27:34 +0000

    afficher les variables et leurs taille

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - test c
+ *
+ *Return: toujours (0)
+ */
+int main(void)
+{
+int a;
+float b;
+long int c;
+long long int d;
+char e;
+	printf("size of int: %zu bytes\n", (unsigned long)sizeof(a));
+	printf("size of float: %zu bytes\n", (unsigned long)sizeof(b));
+	printf("size of long int: %zu bytes\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %zu bytes\n", (unsigned long)sizeof(d));
+	printf("size of char: %zu bytes\n", (unsigned long)sizeof(e));
+	return (0);
+}
