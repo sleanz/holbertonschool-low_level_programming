91145adea693b67124c206681a7dc8b282cc3a99
sleanz <nils.8@hotmail.fr>
2024-10-21 12:50:02 +0000

    afficher les variables et leurs taille

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ float b;
 long int c;
 long long int d;
 char e;
+	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(e));
 	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of long long init: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
