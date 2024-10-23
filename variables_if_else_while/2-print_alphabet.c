ca9cab2998858eb15aa7f31028971de8a01e4641
sleanz <nils.8@hotmail.fr>
2024-10-23 08:56:53 +0000

    commandes putachar , compter alpahbet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - test c
+ *
+ *Return: toujours (0)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
