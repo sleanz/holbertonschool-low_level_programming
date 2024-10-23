be1715e24b551ea2b0a7c61a1df720f15f3091f2
sleanz <nils.8@hotmail.fr>
2024-10-23 11:34:36 +0000

    afficher des chiffres de 0 a 9 d ordre croissant avec des virgules et espaces

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - test c
+ *
+ *Return: toujours (0)
+ */
+int main(void)
+{
+	int  ch;
+
+	for (ch = '0'; ch < 10 ; ch++)
+		putchar(ch + '0'); /* affiche le chiffre actuel*/
+			if (ch < 9) /* verifie que ce n'est pas le dernier chiffre*/
+				putchar(','); /*ajoute une ,*/
+	putchar(' '); /* ajoute un espace*/
+	putchar('\n');
+	return (0);
+}
+
+
