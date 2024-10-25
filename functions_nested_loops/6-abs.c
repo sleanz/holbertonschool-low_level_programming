1f70ff2bff83be8918bb696bef64b7e8802d8ddd
sleanz <nils.8@hotmail.fr>
2024-10-25 14:14:30 +0000

    afficher la valeur absolue

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Check mainh
+ * @r: An integer number
+ * Description: function prints the absolute value
+ * Return: 0 if its a absolute value and 1 if its not
+ */
+int _abs(int r)
+{
+
+	if (r >=  0)
+		return (r);
+	else
+		return (r * -1);
+
+}
