a451a360d9a67e6c26aaea2dcadc82960f42b099
sleanz <nils.8@hotmail.fr>
2024-10-25 10:10:52 +0000

    iimprime l alphabet en x10

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,3 +14,6 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
