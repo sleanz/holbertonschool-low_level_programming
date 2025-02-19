af9e0c7fa81cae3d2299f47653566ba1aa94b1b5
sleanz <nils.8@hotmail.fr>
2025-02-19 10:48:43 +0100

    variable et loop en c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char min;
+
+	for (min = 'a'; min <= 'z'; min++)
+	{
+		putchar(min);
+	}
+	putchar('\n');
+
+	return (0);
+}
