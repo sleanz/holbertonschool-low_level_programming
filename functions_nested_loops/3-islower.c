680c64de7ec4f2c56178149f681e8816a5678c77
sleanz <nils.8@hotmail.fr>
2025-02-22 20:05:55 +0100

    fonction loop

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,21 @@
 #include "main.h"
 /**
- * _islower - checks for lowercase character
- *
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _islower - Check main
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+
+	return (lower);
 }
